7c202d478c58f771899245a72417b410e8445fbb
chiennv <chiennguyen16112003@gmail.com>
2026-02-10 14:44:28 +0700

    feat: auth

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,14 +1,13 @@
-import { Body, Controller, Post } from "@nestjs/common";
-import { CreateUserDto } from "./dto/user.dto";
-import { UserService } from "./user.service";
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/user.dto';
+import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
-    constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) {}
 
-    @Post()
-    async createUser(@Body() data: CreateUserDto) {
-        return await this.userService.create(data);
-    }
-
+  @Post()
+  async createUser(@Body() data: CreateUserDto) {
+    return await this.userService.create(data);
+  }
 }
