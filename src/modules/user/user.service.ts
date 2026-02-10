e738c663d4be8c8e7f0e0225d4f0cb329e65f089
chiennv <chiennguyen16112003@gmail.com>
2026-02-10 14:50:31 +0700

    fix: orm

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { BaseService } from 'src/common/base/base.service';
-import { User } from 'src/database/entities/user.entity';
+import { BaseService } from '../../common/base/base.service';
+import { User } from '../../database/entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/user.dto';
 
